d3e34d1bf82bdb60b6c25b8a6f667293190b81a9
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2021-04-30 11:23:39 +0900

    Fix solc version

--- contracts/SCVxACS.sol
+++ contracts/SCVxACS.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.7.6;
 
 import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
